cb00467306b283b46d0ee9790a8aae15c566e192
Julie Goncalves <julie.g27@hotmail.fr>
2023-08-18 23:49:29 +0200

    En cours - travail sur le texte de la carre "a propos"

--- js/palette.js
+++ js/palette.js
@@ -1,6 +1,6 @@
 var elementsDeCouleur = document.querySelectorAll(".changeCouleur");
 var btnBleu = document.getElementById("palette1");
-var btnNoir = document.getElementById("palette2");
+var btnBlan = document.getElementById("palette2");
 var btnRose = document.getElementById("palette3");
 
 var paletteActive = "rose";
@@ -12,7 +12,7 @@ btnBleu.addEventListener("click", function () {
 
   elementsDeCouleur.forEach(function (element) {
     element.classList.remove("rose");
-    element.classList.remove("noir");
+    element.classList.remove("blan");
     element.classList.add("bleu");
   });
   paletteActive = "bleu";
@@ -22,22 +22,22 @@ btnRose.addEventListener("click", function () {
   if (paletteActive == "rose") {
     return;
   }
-  elementsDeCouleur.forEach(function(element) {
-  element.classList.remove("noir");
-  element.classList.remove("bleu");
-  element.classList.add("rose");
-});
-paletteActive = "rose";
+  elementsDeCouleur.forEach(function (element) {
+    element.classList.remove("blan");
+    element.classList.remove("bleu");
+    element.classList.add("rose");
+  });
+  paletteActive = "rose";
 });
 
-btnNoir.addEventListener("click", function(){
-if (paletteActive == "noir") {
-return;
-}
-elementsDeCouleur.forEach(function(element){
-  element.classList.remove("bleu");
-  element.classList.remove("rose");
-  element.classList.add("noir");
-});
-paletteActive = "noir";
+btnBlan.addEventListener("click", function () {
+  if (paletteActive == "blan") {
+    return;
+  }
+  elementsDeCouleur.forEach(function (element) {
+    element.classList.remove("bleu");
+    element.classList.remove("rose");
+    element.classList.add("blan");
+  });
+  paletteActive = "blan";
 });

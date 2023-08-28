8b9a4c86ac0d6d215d5674f9a355506f3fd11ba0
Julie Goncalves <julie.g27@hotmail.fr>
2023-08-28 16:22:03 +0200

    Medias queries - section projet

--- js/fond-motifs/fond-motifs.js
+++ js/fond-motifs/fond-motifs.js
@@ -1,26 +1,26 @@
 
-var cle = "etoile"; 
-var soleil = "soleil";
-var lune = "lune";
-var oeil = "oeil";
-var motifs = [cle, oeil, lune, soleil];
-var copy = 25; // Nombre de copie à faire
+// var cle = "etoile"; 
+// var soleil = "soleil";
+// var lune = "lune";
+// var oeil = "oeil";
+// var motifs = [cle, oeil, lune, soleil];
+// var copy = 25; // Nombre de copie à faire
 
-var gridContainer = document.querySelector(".grid-2");
+// var gridContainer = document.querySelector(".grid-2");
 
-for (var i = 0; i < copy; i++) {
-  for (var j = 0; j < motifs.length; j++) {
-    var cell = document.createElement("div");
-    cell.classList.add("grid-cell");
+// for (var i = 0; i < copy; i++) {
+//   for (var j = 0; j < motifs.length; j++) {
+//     var cell = document.createElement("div");
+//     cell.classList.add("grid-cell");
     
-    var imageDiv = document.createElement("div");
-    imageDiv.classList.add("image-container");
-    var image = document.createElement("img");
-    image.src = "js/fond-motifs/" + motifs[j] + ".svg"; // Assurez-vous d'avoir les fichiers SVG dans le même répertoire que votre script
+//     var imageDiv = document.createElement("div");
+//     imageDiv.classList.add("image-container");
+//     var image = document.createElement("img");
+//     image.src = "js/fond-motifs/" + motifs[j] + ".svg"; // Assurez-vous d'avoir les fichiers SVG dans le même répertoire que votre script
     
-    imageDiv.appendChild(image);
-    cell.appendChild(imageDiv);
+//     imageDiv.appendChild(image);
+//     cell.appendChild(imageDiv);
     
-    gridContainer.appendChild(cell);
-  }
-}
+//     gridContainer.appendChild(cell);
+//   }
+// }
--- js/medias-queries.js
+++ js/medias-queries.js
@@ -1,2 +1,37 @@
-// var gridItem = document.querySelector('.grid-row-4-column-1');
-// gridItem.classList.add('.grid-row-4-column-3');
+// J aimerais modifier "var copy = 25" par var "var copy = 50" lorsque je change de format d ecran (plus petit)
+
+var cle = "etoile";
+var soleil = "soleil";
+var lune = "lune";
+var oeil = "oeil";
+var motifs = [cle, oeil, lune, soleil];
+var largeur = window.innerWidth;
+var gridContainer = document.querySelector(".grid-2");
+
+function rajouterMotifs(largeurDeFenetre) {
+  var copy = 25; // Nombre de copie à faire
+  if (largeurDeFenetre <= 420) {
+    copy = 50;
+  }
+
+  // J ajoute 1 tant que i est inferieur a copy
+  for (var i = 0; i < copy; i++) {
+    // J ajoute 1 tant que je n ai pas 4 motifs
+    for (var j = 0; j < motifs.length; j++) {
+      var cell = document.createElement("div");
+      cell.classList.add("grid-cell");
+
+      var imageDiv = document.createElement("div");
+      imageDiv.classList.add("image-container");
+      var image = document.createElement("img");
+      image.src = "js/fond-motifs/" + motifs[j] + ".svg";
+
+      imageDiv.appendChild(image);
+      cell.appendChild(imageDiv);
+
+      gridContainer.appendChild(cell);
+    }
+  }
+}
+
+rajouterMotifs(largeur);

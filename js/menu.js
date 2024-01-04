b7ac101393e3d8537056a5568fdd4bc48e5c44ca
Julie Goncalves <julie.g27@hotmail.fr>
2024-01-04 14:46:18 +0100

    mouseenter/mouseout - fonctionnel

--- js/menu.js
+++ js/menu.js
@@ -9,7 +9,9 @@
     var containerHeight = $(".container").outerHeight();
     var containerWidth = $(".container").outerWidth();
 
+    // quand je sors
     var containerMenu = document.querySelector(".container");
+
     containerMenu.addEventListener("mouseout", function (eventMouseMove) {
       // document.addEventListener("mousemove", function (eventMouseMove) {
       const position = $(".container").offset();
@@ -17,50 +19,40 @@
       theLeft = position.left;
       // console.log(theLeft);
 
-      if (
-        eventMouseMove.pageX < theLeft ||
-        eventMouseMove.pageX > theLeft + containerWidth
-      ) {
-        console.log("a l exterieur horizontal");
-        // Quand ma souris est a l exterieur de notre container on veut que notre zone de floue haute prenne toute la hauteur du container, et la zone de floue du bas soit a 0.
+      console.log("a l exterieur horizontal");
+      // Quand ma souris est a l exterieur de notre container on veut que notre zone de floue haute prenne toute la hauteur du container, et la zone de floue du bas soit a 0.
 
-        isMouseOutsideContainer = true;
-        $(".top-side").css({
-          height: containerHeight,
-        });
-        $(".bottom-side").css({
-          height: 0,
-        });
+      isMouseOutsideContainer = true;
+      $(".top-side").css({
+        height: containerHeight,
+      });
+      $(".bottom-side").css({
+        height: 0,
+      });
+    });
 
+    containerMenu.addEventListener("mouseenter", function (eventMouseMove) {
+      // quand je suis dans le menu
+      // mouse mouve
+      console.log("a l interieur horizontal");
+      // event.page = evenement qui recupere la position de ma souris
+      $(".hover").css({
+        top: eventMouseMove.pageY - theTop, // On veut que la cible commence verticalement au niveau de la souris
+      });
+      $(".top-side").css({
+        height: eventMouseMove.pageY - theTop - focusZoneHeight / 2, // On veut que la zone du haut s'arrete verticalement au niveau de la souris
+      });
 
-        // mouse mouve
-      } else {
-        console.log("a l interieur horizontal");
-        // event.page = evenement qui recupere la position de ma souris
-        $(".hover").css({
-          top: eventMouseMove.pageY - theTop, // On veut que la cible commence verticalement au niveau de la souris
-        });
-        $(".top-side").css({
-          height: eventMouseMove.pageY - theTop - focusZoneHeight / 2, // On veut que la zone du haut s'arrete verticalement au niveau de la souris
-        });
-
-        // On calcule la nouvelle hauteur de la zone du bas
-        var bottomHeight =
-          containerHeight -
-          (eventMouseMove.pageY -
-            theTop +
-            focusZoneHeight -
-            focusZoneHeight / 2);
-        $(".bottom-side").css({
-          top:
-            focusZoneHeight +
-            eventMouseMove.pageY -
-            theTop -
-            focusZoneHeight / 2, // nouvelle posisition verticale de la zone du bas
-          // cad, la position de la souris plus la hauteur de la zone nette
-          height: bottomHeight, // on applique
-        });
-      }
+      // On calcule la nouvelle hauteur de la zone du bas
+      var bottomHeight =
+        containerHeight -
+        (eventMouseMove.pageY - theTop + focusZoneHeight - focusZoneHeight / 2);
+      $(".bottom-side").css({
+        top:
+          focusZoneHeight + eventMouseMove.pageY - theTop - focusZoneHeight / 2, // nouvelle posisition verticale de la zone du bas
+        // cad, la position de la souris plus la hauteur de la zone nette
+        height: bottomHeight, // on applique
+      });
     });
   }
   detectMouseMove();
